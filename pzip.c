e0a5a767609b378a6b95663efc3bd1921e551e2c
lowerasdf <bjchandra@wisc.edu>
2021-11-06 15:17:37 -0500

    Non-parallel implementation done

--- pzip.c
+++ pzip.c
@@ -7,4 +7,38 @@ int main(int argc, char *argv[])
         printf("pzip: file1 [file2 ...]\n");
         exit(1);
     }
+
+    char curr;
+    char prev;
+    int count = 1;
+    for(int i = 1; i < argc; i++) {
+        FILE *f = fopen(argv[i], "r");
+        if(f == NULL) {
+            continue;
+        }
+
+        if (prev == '\0') {
+            prev = fgetc(f);
+        }
+
+        while ((curr = fgetc(f)) != EOF) {
+            if (curr == '\0') {
+                continue;
+            } else if (curr == prev) {
+                count += 1;
+            } else {
+                fwrite(&count, 4, 1, stdout);
+                fwrite(&prev, 1, 1, stdout);
+                count = 1;
+                prev = curr;
+            }
+        }
+
+        fclose(f);
+    }
+    
+    if (prev != '\0') {
+        fwrite(&count, 4, 1, stdout);
+        fwrite(&prev, 1, 1, stdout);
+    }
 }
