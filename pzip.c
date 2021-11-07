c238aba019051dc58dab228eed5110567ce8e075
lowerasdf <bjchandra@wisc.edu>
2021-11-07 13:40:37 -0600

    Producer and consumer idea

--- pzip.c
+++ pzip.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/sysinfo.h>
+
+#define MAX_BUFFER_SIZE 100000
+
+int n_threads;
+int n_files;
+
+int q_head_idx = 0;
+int q_tail_idx = 0;
+int q_size = 0;
+
+struct buffer {
+    char *value;
+    int index;
+    int size;
+};
+
+struct data {
+    char *keyword;
+    int *count;
+};
+
+struct buffer *queue;
+struct data *results;
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +32,15 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
+    n_threads = get_nprocs();
+    n_files = argc - 1;
+
+    queue = malloc(n_threads * sizeof(struct buffer));
+    results = malloc(n_files * sizeof(struct data));
+
+    // TODO create pthread for 1 producer and n_threads consumers
+
+    // TODO do mmap in producer, parse in consumer, print output after join
     char curr;
     char prev;
     int count = 1;
@@ -41,4 +74,9 @@ int main(int argc, char *argv[])
         fwrite(&count, 4, 1, stdout);
         fwrite(&prev, 1, 1, stdout);
     }
+
+    // TODO free inside
+
+    free(queue);
+    free(results);
 }
